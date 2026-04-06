6d87b642095c279ef2df6f5e04aba3a2f2dd9feb
silvaaaevelly <evelly.11762423@aluno.mg.gov.br>
2026-04-06 08:32:59 -0300

    Implement caminhar and dormir methods in Pascoa1
    Added methods for walking and sleeping in Pascoa1 class.

---
+++ classePessoa.java
@@ -0,0 +1,13 @@
+package pascoa;
+public class Pascoa1 {
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        public void caminhar () {
+            System.out.println("caminhando");
+        }
+        public void dormir (){
+            System.out.println ("dormir");
+        }
+    }
+}
