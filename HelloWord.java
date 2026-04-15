f06d1e9e478396bbff2b19e4c7de34ca36499ca7
silvaaaevelly <evelly.11762423@aluno.mg.gov.br>
2026-04-15 13:00:38 -0300

    HelloWorld

---
+++ HelloWord.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author SEEMG
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
